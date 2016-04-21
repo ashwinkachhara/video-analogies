c7a31e9ca9e08d33e20400e1eaba7eed151830ba
Bhuvanesh <bhuvanesh_shan@rocketmail.com>
2016-04-21 12:31:24 -0400

    coherence match

--- analogies.py
+++ analogies.py
@@ -67,7 +67,7 @@ class Analogies:
     def bestMatch(self,q):
         p_app = self.bestApproximateMatch(q)
         return p_app
-        # p_coh = self.bestCoherenceMatch(q)
+        (p_coh, d_coh) = self.bestCoherenceMatch(q)
         # # d_app
         # # d_coh
         # if d_coh <= d_app*(1+0.5*self.K):
@@ -75,7 +75,7 @@ class Analogies:
         # else:
         #     return p_app
 
-    def bestApproximateMatch(self,q):
+    def bestApproximateMatch(self, q):
         # v = feature at q
         x,y = self.LinearToXY(q,self.bshape)
         if x>=self.bshape[0] or y>=self.bshape[1]:
@@ -83,5 +83,41 @@ class Analogies:
         v = featureVector.getFeatureVectorForRowCol(self.B.reshape(self.bshape),self.B1.reshape(self.bshape),x,y)
         return self.ann.query(v)
 
-    # def bestCoherenceMatch(self,q):
-    #     # in nbd of q
+    def bestCoherenceMatch(self, q):
+        # in nbd of q
+        x,y = self.LinearToXY(q,self.bshape)
+        if x>=self.bshape[0] or y>=self.bshape[1]:
+            print "out of bounds",x,y,q
+        fvq = featureVector.getFeatureVectorForRowCol(self.B.reshape(self.bshape),self.B1.reshape(self.bshape),x,y)
+        minNeighbor = None
+        minDiff = 13*99 # A random high value
+        #top 3 neighbors
+        for l in range(-1, 2):
+            i = (x - 1) % self.bshape[0]
+            j = (y + l) % self.bshape[1]
+            fvij = featureVector.getFeatureVectorForRowCol(self.B.reshape(self.bshape),self.B1.reshape(self.bshape),i,j)
+            diff = self.getDiff(fvq, fvij)
+            if diff < minDiff:
+                minDiff = diff
+                minNeighbor = self.XYToLinear(i, j, self.B1)
+        #left neighbor
+        i = x;
+        j = (y-1)%self.bshape[1]
+        fvij = featureVector.getFeatureVectorForRowCol(self.B.reshape(self.bshape),self.B1.reshape(self.bshape),i,j)
+        diff = self.getDiff(fvq, fvij)
+        if diff < minDiff:
+            minDiff = diff
+            minNeighbor = self.XYToLinear(i, j, self.B1)
+        return (minNeighbor, minDiff)
+
+
+
+    def getDiff(fv1, fv2);
+        diffVec = np.array(fv1) - np.array(fv2)
+        diffVec = diffVec**2
+        diff = sum(diffVec)
+        return diff
+
+
+
+
